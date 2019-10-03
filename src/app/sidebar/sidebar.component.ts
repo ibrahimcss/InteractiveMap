2cd33836a23d583d0c6bd1ade74943b19221f7b9
ibrahimcss <ibrahimcss@outlook.com>
2019-10-03 22:36:52 +0300

    sidebar code to be edited

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,7 +10,7 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $.getScript('../src/assets/js/sidebar.js');
+    $.getScript('src/assets/js/sidebar.js');
 
   }
 
